a2f4e9b72ff659d2ae1281fcbdbc10cb8d5b3295
Adam Kidder <adam@highfive.com>
2018-12-08 13:56:53 -0800

    Show stats panel if ?debug url param is given

--- src/terrarium.js
+++ src/terrarium.js
@@ -1,6 +1,9 @@
 import App from './app.js';
 import Game from './game.js';
 
-const app = new App('Terrarium', new Game());
+const urlParams = new URLSearchParams(window.location.search);
+const debug = urlParams.has('debug');
+
+const app = new App('Terrarium', new Game(), { debug: debug });
 app.setEventListeners();
 app.run();
